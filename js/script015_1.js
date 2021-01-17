c69d80e53ff4711471be9aac5517aa6be3bdc0a9
Romas <kovalevroman@mail.ru>
2021-01-17 22:52:40 +0300

    Скрипт 15 с правками

--- js/script015_1.js
+++ js/script015_1.js
@@ -1,14 +1,31 @@
 'use strict';
 alert('Код - вопросы пользователю про фильмы с помощью циклов');
-const lengthStr=50
+const lengthStr=20;
+const numberOfFilms = +prompt('Сколько фильмов уже просмотрели?', '');//вопрос на форме с преобразованием значения в целое число +prompt
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies:{},//пустой объект
+    actors:{},//пустой объкт
+    genders:[],//пустой массив
+    privat: false//логический тип с присвоенным значением
+};
+
 for (let i=0; i<2; i++)
-{
-    let film=prompt('Название вашего любимого фильма: ', '');
-    if (film.length < lengthStr && film != null && film != '') {
-        alert('Условия меня устраивают');
-        console,log(film);
+    {
+        const   film = prompt('Название вашего любимого фильма: ', ''),
+                b=prompt('Оценка фильму', '');
+        if (film.length < lengthStr && film != null && film != '' && b != null && b != '')
+        {
+            alert('Условия меня устраивают');
+            personalMovieDB.movies[film]=b;
+            console.log(film);
+            i++;
+        }
+        else {
+            alert('Повторите ввод снова');
+            console.log('Error');
+            i--;
+            }
     }
-    else {alert('Повторите ввод снова');
-         i--;}
-      
-}
+console.log(personalMovieDB);
+
